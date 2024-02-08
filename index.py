0c4760553f75339237e7898a18f0b4edb59e294a
valdez101 <jvaldez0754@gmail.com>
2024-02-08 18:59:33 -0500

    test cd

---
+++ index.py
@@ -0,0 +1,26 @@
+
+
+from kfp.v2.dsl import pipeline, component, Artifact, Dataset, Input, Metrics, Model, Output, InputPath, OutputPath
+
+@component(
+    packages_to_install=["pandas", "pyarrow", "scikit-learn"],
+    base_image ="python:3.10",
+    output_component_file="dataset_creating_1.yaml"
+)
+def get_data_from_bq(
+    output_data_path: OutputPath("Dataset"),
+    bq_table: Optional[str] = None
+):
+
+    import pandas as pd
+    from sklearn.datasets import load_iris
+
+# Cargar el conjunto de datos Iris
+    iris = load_iris()
+    X, Y = iris.data, iris.target
+
+# Crear un DataFrame de Pandas con las características y las etiquetas
+    df = pd.DataFrame(X, columns=iris.feature_names)
+    df['target'] = Y
+
+    df.to_csv(output_data_path, index=False)
